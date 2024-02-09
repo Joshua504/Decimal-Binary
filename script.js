c10be36d4aebf60882a0afb31c913801e4dcd24d
Joshua504 <chukwuebukajosh8@gmail.com>
2024-02-09 23:11:42 +0100

    decimal-binary- test 1

---
+++ script.js
@@ -0,0 +1,35 @@
+const numberInput = document.getElementById("number-input");
+const convertBtn = document.getElementById("convert-btn");
+const result = document.getElementById("result");
+
+function decimalToBinary(input) {
+	let binary = "";
+	if (input === 0) {
+		binary = "0";
+	}
+	while (input > 0) {
+		input = Math.floor(input / 2);
+		binary = (input % 2) + binary;
+	}
+	result.innerText = binary;
+}
+
+function checkUserInput() {
+	if (!numberInput.value || isNaN(parseInt(numberInput.value))) {
+		alert("Please provide a decimal number");
+		return;
+	}
+	decimalToBinary(parseInt(numberInput.value));
+	numberInput.value = "";
+}
+
+convertBtn.addEventListener("click", checkUserInput);
+numberInput.addEventListener("keydown", (e) => {
+	if (e.key === "Enter") {
+		checkUserInput();
+	}
+});
+
+function countdown(number) {
+  console.log(number);
+}
